bd5a36a9d85a41d9bb56d15bc1668d8751d66274
nelliemckesson <nellie.mckesson@macmillan.com>
2015-05-29 09:10:16 -0400

    adding pdfmaker processing

---
+++ pdfmaker_preprocessing.rb
@@ -0,0 +1,45 @@
+# images need to go in a predictable folder for bookmaker sourcing. maybe just a pdfimg folder, right at the outset? then the original images are preserved.
+
+imgtmp_dir = File.join(Bkmkr::Paths.project_tmp_dir_img, "imgtmp")
+
+#if any images are in 'done' dir, grayscale and upload them to macmillan.tools site
+images = Dir.entries("#{Bkmkr::Paths.project_tmp_dir_img}").select {|f| !File.directory? f}
+image_count = images.count
+if image_count > 0
+	#using this model for Fileutils.cp to select all files in a dir (* won't work):  FileUtils.cp Dir["#{dir1}/*"].select {|f| test ?f, f}, "#{dir2}"
+	FileUtils.cp Dir["#{Bkmkr::Paths.project_tmp_dir_img}/*"].select {|f| test ?f, f}, pdftmp_dir
+	pdfimages = Dir.entries("#{Bkmkr::Paths.project_tmp_dir_img}/pdftmp").select { |f| !File.directory? f }
+	pdfimages.each do |i|
+		pdfimage = File.join(pdftmp_dir, "#{i}")
+		if i.include?("fullpage")
+			#convert command for ImageMagick should work the same on any platform
+			`convert "#{pdfimage}" -colorspace gray "#{pdfimage}"`
+		elsif i.include?("_FC") or i.include?(".txt") or i.include?(".css") or i.include?(".js")
+			FileUtils.rm("#{pdfimage}")
+		else
+			myres = `identify -format "%y" "#{pdfimage}"`
+			myres = myres.to_f
+			myheight = `identify -format "%h" "#{pdfimage}"`
+			myheight = myheight.to_f
+			myheightininches = ((myheight / myres) * 72.0)
+			mywidth = `identify -format "%h" "#{pdfimage}"`
+			mywidth = mywidth.to_f
+			mywidthininches = ((mywidth / myres) * 72.0)
+			if mywidthininches > 3.5 or myheightininches > 5.5 then
+				targetheight = 5.5 * myres
+				targetwidth = 3.5 * myres
+				`convert "#{pdfimage}" -resize "#{targetwidth}x#{targetheight}>" "#{pdfimage}"`
+			end
+			myheight = `identify -format "%h" "#{pdfimage}"`
+			myheight = myheight.to_f
+			myheightininches = ((myheight / myres) * 72.0)
+			mymultiple = ((myheight / myres) * 72.0) / 16.0
+			if mymultiple <= 1
+				`convert "#{pdfimage}" -colorspace gray "#{pdfimage}"`
+			else 
+				newheight = ((mymultiple.floor * 16.0) / 72.0) * myres
+				`convert "#{pdfimage}" -resize "x#{newheight}" -colorspace gray "#{pdfimage}"`
+			end
+		end
+	end
+end
