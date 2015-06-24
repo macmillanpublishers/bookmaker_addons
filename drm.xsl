3f59cd5c9f2273c58173104baeaf74817b60ab0b
nelliemckesson <nellie.mckesson@macmillan.com>
2015-06-24 09:32:03 -0400

    adding drm and backads to epubs

---
+++ drm.xsl
@@ -0,0 +1,30 @@
+<xsl:stylesheet version="1.0" 
+		xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+		xmlns:h="http://www.w3.org/1999/xhtml"
+		xmlns="http://www.w3.org/1999/xhtml"
+		exclude-result-prefixes="h">
+
+<xsl:output method="xml"
+            encoding="UTF-8"/>
+<xsl:preserve-space elements="*"/>
+
+<xsl:param name="drmnotice">
+    <section data-type="preface" class="drm">
+        <h1 class="Nonprinting">Copyright Notice</h1>
+        <p>The author and publisher have provided this e-book to you without Digital Rights Management software (DRM) applied so that you can enjoy reading it on your personal devices. This e-book is for your personal use only. You may not print or post this e-book, or make this e-book publicly available in any way. You may not copy, reproduce or upload this e-book, other than to read it on one of your personal devices.</p>
+        <p><strong>Copyright infringement is against the law. If you believe the copy of this e-book you are reading infringes on the author’s copyright, please notify the publisher at: <a href="http://us.macmillanusa.com/piracy">http://us.macmillanusa.com/piracy</a>.</strong></p>    
+    </section>
+ </xsl:param>
+
+<xsl:template match="node()|@*" name="identity">
+     <xsl:copy>
+       <xsl:apply-templates select="node()|@*"/>
+     </xsl:copy>
+ </xsl:template>
+
+<xsl:template match="h:section[@data-type='titlepage'][last()]">
+	<xsl:call-template name="identity"/>
+  	<xsl:copy-of select="$drmnotice"/>
+</xsl:template>
+
+</xsl:stylesheet> 
--- epubmaker_preprocessing.rb
+++ epubmaker_preprocessing.rb
@@ -17,7 +17,17 @@ assets_dir = File.join(Bkmkr::Paths.scripts_dir, "bookmaker_assets", "epubmaker"
 
 # Adding imprint logo to title page
 # Removing images subdir from src attr
-filecontents = File.read(Bkmkr::Paths.outputtmp_html).gsub(/<p class="TitlepageImprintLineimp">/,"<img src=\"logo.jpg\"/><p class=\"TitlepageImprintLineimp\">").gsub(/src="images\//,"src=\"")
+# inserting imprint backad, if it exists
+backad_file = File.join(assets_dir, "images", project_dir, "backad.jpg")
+
+if File.file?(backad_file)
+  backad = "<section data-type='colophon'><img src='backad.jpg' alt='advertising'/></section>"
+else
+  backad = ""
+end
+
+filecontents = File.read(Bkmkr::Paths.outputtmp_html).gsub(/<p class="TitlepageImprintLineimp">/,"<img src=\"logo.jpg\"/><p class=\"TitlepageImprintLineimp\">").gsub(/src="images\//,"src=\"").gsub(/<\/body>/,"#{backad}</body>").gsub(/(<section data-type="chapter".*?><h1.*?>)(.*?)(<\/h1>)/, "\\1<a href=\"toc01.html\">\\2</a>\\3")
+
 # Update several copyright elements for epub
 if filecontents.include?('data-type="copyright-page"')
   copyright_txt = filecontents.match(/(<section data-type=\"copyright-page\" .*?\">)((.|\n)*?)(<\/section>)/)[2]
@@ -42,8 +52,12 @@ end
 
 # strip halftitlepage from html
 strip_halftitle_xsl = File.join(Bkmkr::Paths.core_dir, "epubmaker", "strip-halftitle.xsl")
+# insert DRM copyright notice in HTML
+drm_xsl = File.join(Bkmkr::Paths.scripts_dir, "bookmaker_addons", "drm.xsl")
 
 `java -jar "#{saxonpath}" -s:"#{epub_tmp_html}" -xsl:"#{strip_halftitle_xsl}" -o:"#{epub_tmp_html}"`
+
+`java -jar "#{saxonpath}" -s:"#{epub_tmp_html}" -xsl:"#{drm_xsl}" -o:"#{epub_tmp_html}"`
 
 #set logo image based on project directory
 logo_img = File.join(assets_dir, "images", project_dir, "logo.jpg")
