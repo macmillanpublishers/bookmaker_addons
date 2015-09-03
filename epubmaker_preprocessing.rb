c4408ad6dec8942fe70b8de61a78163c80cc5a15
nelliemckesson <nellie.mckesson@macmillan.com>
2015-09-03 16:28:41 -0400

    adding titlepage image hadnling

--- epubmaker_preprocessing.rb
+++ epubmaker_preprocessing.rb
@@ -58,6 +58,7 @@ end
 images = Dir.entries(Bkmkr::Paths.submitted_images)
 finalimagedir = File.join(Bkmkr::Paths.done_dir, Metadata.pisbn, "images")
 imgstring = images.join(",")
+puts imgstring
 etpfilename = imgstring.match(/epubtitlepage.[a-zA-Z]*/).to_s
 puts etpfilename
 
