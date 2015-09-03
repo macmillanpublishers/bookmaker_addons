234e67ec8b0fdced7f9d6dd3cbf7b3837cfd2771
nelliemckesson <nellie.mckesson@macmillan.com>
2015-09-03 15:50:54 -0400

    adding titlepage image hadnling

--- epubmaker_preprocessing.js
+++ epubmaker_preprocessing.js
@@ -15,7 +15,7 @@ fs.readFile(file, function editContent (err, contents) {
   	header = '<h1 class="Nonprinting">Title Page</h1>';
   	$('section[data-type="titlepage"]').prepend(header);
   	//add image holder
-  	image = '<img src="ebooktitlepage.jpg"/>';
+  	image = '<img src="epubtitlepage.jpg"/>';
   	$('section[data-type="titlepage"]').append(image);
   }
 
