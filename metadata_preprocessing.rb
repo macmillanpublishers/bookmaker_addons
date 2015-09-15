603b9e3276685984f1936ee4683c627c78170fff
nelliemckesson <nellie.mckesson@macmillan.com>
2015-09-15 13:03:35 -0400

    removing ref to metadata

--- metadata_preprocessing.rb
+++ metadata_preprocessing.rb
@@ -53,7 +53,7 @@ end
 # find titlepage images
 
 images = File.join(Bkmkr::Paths.submitted_images, pisbn, "images")
-allimg = File.join(finalimagedir, "*")
+allimg = File.join(images, "*")
 etparr = Dir[allimg].select { |f| f.include?('epubtitlepage.')}
 ptparr = Dir[allimg].select { |f| f.include?('titlepage.')}
 
