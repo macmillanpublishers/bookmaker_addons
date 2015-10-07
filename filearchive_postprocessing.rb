d7bace4af355abc74715ce838cd8a0f71e0bfe8e
nelliemckesson <nellie.mckesson@macmillan.com>
2015-10-07 10:53:01 -0400

    adding unless for titlepages

--- filearchive_postprocessing.rb
+++ filearchive_postprocessing.rb
@@ -11,11 +11,15 @@ finalimagedir = File.join(Bkmkr::Paths.done_dir, Metadata.pisbn, "images")
 if File.file?(Metadata.podtitlepage)
 	ptpfilename = Metadata.podtitlepage.split(Regexp.union(*[File::SEPARATOR, File::ALT_SEPARATOR].compact)).pop
 	podtitlepagearc = File.join(finalimagedir, ptpfilename)
-	FileUtils.mv(Metadata.podtitlepage, podtitlepagearc)
+	unless Metadata.podtitlepage == podtitlepagearc
+		FileUtils.mv(Metadata.podtitlepage, podtitlepagearc)
+	end
 end
 
 if File.file?(Metadata.epubtitlepage)
 	etpfilename = Metadata.epubtitlepage.split(Regexp.union(*[File::SEPARATOR, File::ALT_SEPARATOR].compact)).pop
 	epubtitlepagearc = File.join(finalimagedir, etpfilename)
-	FileUtils.mv(Metadata.epubtitlepage, epubtitlepagearc)
+	unless Metadata.epubtitlepage == epubtitlepagearc
+		FileUtils.mv(Metadata.epubtitlepage, epubtitlepagearc)
+	end
 end
