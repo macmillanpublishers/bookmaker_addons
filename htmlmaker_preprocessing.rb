8f5f7a04af32d733b842974e2b41ed1a048877ff
nelliemckesson <nellie.mckesson@macmillan.com>
2015-11-02 12:36:34 -0500

    adding new htmlmaker preprocessing step

--- htmlmaker_preprocessing.rb
+++ htmlmaker_preprocessing.rb
@@ -1,6 +1,6 @@
 require 'fileutils'
 
-require_relative '../bookmaker/header.rb'
+require_relative '../header.rb'
 
 # These commands should run immediately prior to htmlmaker
 doctodocx = "S:\\resources\\bookmaker_scripts\\bookmaker_addons\\htmlmaker_preprocessing.ps1"
