8894ae67ac3d6dfa121c7fe1e95850a174ec9dbd
nelliemckesson <nellie.mckesson@macmillan.com>
2015-11-02 12:41:35 -0500

    fixing var ref

--- htmlmaker_preprocessing.rb
+++ htmlmaker_preprocessing.rb
@@ -4,4 +4,4 @@ require_relative '../bookmaker/core/header.rb'
 
 # These commands should run immediately prior to htmlmaker
 doctodocx = "S:\\resources\\bookmaker_scripts\\bookmaker_addons\\htmlmaker_preprocessing.ps1"
-`PowerShell -NoProfile -ExecutionPolicy Bypass -Command "#{doctodocx} '#{Bkmkr::Project.project_tmp_file}'"`
+`PowerShell -NoProfile -ExecutionPolicy Bypass -Command "#{doctodocx} '#{Bkmkr::Paths.project_tmp_file}'"`
