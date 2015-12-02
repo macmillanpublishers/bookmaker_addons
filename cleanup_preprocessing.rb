30bcf82ea3bfa29a105cfeef8be09ce7f8231202
nelliemckesson <nellie.mckesson@macmillan.com>
2015-12-02 09:44:58 -0500

    adding cleanup preprocessing

---
+++ cleanup_preprocessing.rb
@@ -0,0 +1,33 @@
+require 'net/ftp'
+
+require_relative '../bookmaker/core/header.rb'
+require_relative '../bookmaker/core/metadata.rb'
+
+data_hash = Bkmkr::Tools.readjson(Metadata.configfile)
+project_dir = data_hash['project']
+stage_dir = data_hash['stage']
+
+# clean up the ftp site if files were uploaded
+uploaded_image_log = "#{Bkmkr::Paths.project_tmp_dir_img}/uploaded_image_log.txt"
+filexist = Bkmkr::Tools.checkFileExist(uploaded_image_log)
+fileempty = Bkmkr::Tools.checkFileEmpty(uploaded_image_log)
+
+ftp_username = Bkmkr::Tools.readFile("#{$scripts_dir}/bookmaker_authkeys/ftp_username.txt")
+ftp_password = Bkmkr::Tools.readFile("#{$scripts_dir}/bookmaker_authkeys/ftp_pass.txt")
+
+def deleteFTP(parentfolder, childfolder)
+  ftp = Net::FTP.new('142.54.232.104')
+  ftp.login(user = "#{ftp_username}", passwd = "#{ftp_password}")
+  files = ftp.chdir("/files/html/bookmaker/bookmakerimg/#{parentfolder}/#{childfolder}")
+  filenames = ftp.nlst()
+  filenames.each do |d|
+    file = ftp.delete(d)
+  end
+  files = ftp.nlst()
+  ftp.close
+  files
+end
+
+if filexist == true && fileempty == false
+  deleteFTP("#{project_dir}_#{stage_dir}", Metadata.pisbn)
+end
