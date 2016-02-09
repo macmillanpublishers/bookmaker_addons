feb08ab50b6aa0abc5f8f9b662d44fa316e3f223
nelliemckesson <nellie.mckesson@macmillan.com>
2016-02-09 09:27:33 -0500

    adding handling for metadata overrides

--- metadata_preprocessing.rb
+++ metadata_preprocessing.rb
@@ -116,8 +116,16 @@ else
 	puts "No DB record found; falling back to manuscript fields"
 end
 
+metabookauthor = File.read(Bkmkr::Paths.outputtmp_html).scan(/(<meta name="bookauthor" content=")(.*?)("\/>)/)[2]
+metabooktitle = File.read(Bkmkr::Paths.outputtmp_html).scan(/(<meta name="booktitle" content=")(.*?)("\/>)/)[2]
+metabooksubtitle = File.read(Bkmkr::Paths.outputtmp_html).scan(/(<meta name="booksubtitle" content=")(.*?)("\/>)/)[2]
+metapublisher = File.read(Bkmkr::Paths.outputtmp_html).scan(/(<meta name="publisher" content=")(.*?)("\/>)/)[2]
+metaimprint = File.read(Bkmkr::Paths.outputtmp_html).scan(/(<meta name="imprint" content=")(.*?)("\/>)/)[2]
+
 # Finding author name(s)
-if myhash['book'].nil? or myhash['book'].empty? or !myhash['book'] or myhash['book']['WORK_COVERAUTHOR'].nil? or myhash['book']['WORK_COVERAUTHOR'].empty? or !myhash['book']['WORK_COVERAUTHOR']
+if !metabookauthor.nil? or !metabookauthor.empty?
+	authorname = metabookauthor
+elsif myhash['book'].nil? or myhash['book'].empty? or !myhash['book'] or myhash['book']['WORK_COVERAUTHOR'].nil? or myhash['book']['WORK_COVERAUTHOR'].empty? or !myhash['book']['WORK_COVERAUTHOR']
 	authorname = File.read(Bkmkr::Paths.outputtmp_html).scan(/<p class="TitlepageAuthorNameau">.*?</).join(", ").gsub(/<p class="TitlepageAuthorNameau">/,"").gsub(/</,"").gsub(/\[\]/,"")
 else
 	authorname = myhash['book']['WORK_COVERAUTHOR']
@@ -125,7 +133,9 @@ else
 end
 
 # Finding book title
-if myhash['book'].nil? or myhash['book'].empty? or !myhash['book'] or myhash["book"]["WORK_COVERTITLE"].nil? or myhash["book"]["WORK_COVERTITLE"].empty? or !myhash["book"]["WORK_COVERTITLE"]
+if !metabooktitle.nil? or !metabooktitle.empty?
+	booktitle = metabooktitle
+elsif myhash['book'].nil? or myhash['book'].empty? or !myhash['book'] or myhash["book"]["WORK_COVERTITLE"].nil? or myhash["book"]["WORK_COVERTITLE"].empty? or !myhash["book"]["WORK_COVERTITLE"]
 	booktitle = File.read(Bkmkr::Paths.outputtmp_html).scan(/<title>.*?<\/title>/).to_s.gsub(/\["<title>/,"").gsub(/<\/title>"\]/,"").gsub(/\[\]/,"")
 else
 	booktitle = myhash["book"]["WORK_COVERTITLE"]
@@ -133,7 +143,9 @@ else
 end
 
 # Finding book subtitle
-if myhash['book'].nil? or myhash['book'].empty? or !myhash['book'] or myhash["book"]["WORK_SUBTITLE"].nil? or myhash["book"]["WORK_SUBTITLE"].empty? or !myhash["book"]["WORK_SUBTITLE"]
+if !metabooksubtitle.nil? or !metabooksubtitle.empty?
+	booksubtitle = metabooksubtitle
+elsif myhash['book'].nil? or myhash['book'].empty? or !myhash['book'] or myhash["book"]["WORK_SUBTITLE"].nil? or myhash["book"]["WORK_SUBTITLE"].empty? or !myhash["book"]["WORK_SUBTITLE"]
   booksubtitle = File.read(Bkmkr::Paths.outputtmp_html).scan(/<p class="TitlepageBookSubtitlestit">.*?</).join(", ").gsub(/<p class="TitlepageBookSubtitlestit">/,"").gsub(/</,"")
 else
 	booksubtitle = myhash["book"]["WORK_SUBTITLE"]
@@ -147,7 +159,9 @@ stage_dir = Bkmkr::Project.input_file.split(Regexp.union(*[File::SEPARATOR, File
 # Finding imprint name
 # imprint = File.read(Bkmkr::Paths.outputtmp_html).scan(/<p class="TitlepageImprintLineimp">.*?</).to_s.gsub(/\["<p class=\\"TitlepageImprintLineimp\\">/,"").gsub(/"\]/,"").gsub(/</,"")
 # Manually populating for now, until we get the DB set up
-if myhash['book'].nil? or myhash['book'].empty? or !myhash['book'] or myhash["book"]["IMPRINT_DESC"].nil? or myhash["book"]["IMPRINT_DESC"].empty? or !myhash["book"]["IMPRINT_DESC"]
+if !metaimprint.nil? or !metaimprint.empty?
+	imprint = metaimprint
+elsif myhash['book'].nil? or myhash['book'].empty? or !myhash['book'] or myhash["book"]["IMPRINT_DESC"].nil? or myhash["book"]["IMPRINT_DESC"].empty? or !myhash["book"]["IMPRINT_DESC"]
 	if project_dir == "torDOTcom"
 		imprint = "Tom Doherty Associates"
 	elsif project_dir == "SMP"
@@ -160,6 +174,12 @@ if myhash['book'].nil? or myhash['book'].empty? or !myhash['book'] or myhash["bo
 else
 	imprint = myhash["book"]["IMPRINT_DESC"]
 	imprint = imprint.encode('utf-8')
+end
+
+if !metapublisher.nil? or !metapublisher.empty?
+	publisher = metapublisher
+else 
+	publisher = imprint
 end
 
 # print and epub css files
@@ -223,7 +243,7 @@ File.open(configfile, 'w+') do |f|
 	f.puts '"printid":"' + pisbn + '",'
 	f.puts '"ebookid":"' + eisbn + '",'
 	f.puts '"imprint":"' + imprint + '",'
-	f.puts '"publisher":"' + imprint + '",'
+	f.puts '"publisher":"' + publisher + '",'
 	f.puts '"project":"' + project_dir + '",'
 	f.puts '"stage":"' + stage_dir + '",'
 	f.puts '"printcss":"' + pdf_css_file + '",'
