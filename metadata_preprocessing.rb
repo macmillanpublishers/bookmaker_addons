30011c0367acfbf0f760195ed2328d8a480de5b4
nelliemckesson <nellie.mckesson@macmillan.com>
2016-02-09 09:43:15 -0500

    adding handling for metadata overrides

--- metadata_preprocessing.rb
+++ metadata_preprocessing.rb
@@ -123,7 +123,7 @@ metapublisher = File.read(Bkmkr::Paths.outputtmp_html).scan(/(<meta name="publis
 metaimprint = File.read(Bkmkr::Paths.outputtmp_html).scan(/(<meta name="imprint" content=")(.*?)("\/>)/)[2]
 
 # Finding author name(s)
-if !metabookauthor.nil? or !metabookauthor.empty?
+if !metabookauthor.blank?
 	authorname = metabookauthor
 elsif myhash['book'].nil? or myhash['book'].empty? or !myhash['book'] or myhash['book']['WORK_COVERAUTHOR'].nil? or myhash['book']['WORK_COVERAUTHOR'].empty? or !myhash['book']['WORK_COVERAUTHOR']
 	authorname = File.read(Bkmkr::Paths.outputtmp_html).scan(/<p class="TitlepageAuthorNameau">.*?</).join(", ").gsub(/<p class="TitlepageAuthorNameau">/,"").gsub(/</,"").gsub(/\[\]/,"")
@@ -133,7 +133,7 @@ else
 end
 
 # Finding book title
-if !metabooktitle.nil? or !metabooktitle.empty?
+if !metabooktitle.blank?
 	booktitle = metabooktitle
 elsif myhash['book'].nil? or myhash['book'].empty? or !myhash['book'] or myhash["book"]["WORK_COVERTITLE"].nil? or myhash["book"]["WORK_COVERTITLE"].empty? or !myhash["book"]["WORK_COVERTITLE"]
 	booktitle = File.read(Bkmkr::Paths.outputtmp_html).scan(/<title>.*?<\/title>/).to_s.gsub(/\["<title>/,"").gsub(/<\/title>"\]/,"").gsub(/\[\]/,"")
@@ -143,7 +143,7 @@ else
 end
 
 # Finding book subtitle
-if !metabooksubtitle.nil? or !metabooksubtitle.empty?
+if !metabooksubtitle.blank?
 	booksubtitle = metabooksubtitle
 elsif myhash['book'].nil? or myhash['book'].empty? or !myhash['book'] or myhash["book"]["WORK_SUBTITLE"].nil? or myhash["book"]["WORK_SUBTITLE"].empty? or !myhash["book"]["WORK_SUBTITLE"]
   booksubtitle = File.read(Bkmkr::Paths.outputtmp_html).scan(/<p class="TitlepageBookSubtitlestit">.*?</).join(", ").gsub(/<p class="TitlepageBookSubtitlestit">/,"").gsub(/</,"")
@@ -159,7 +159,7 @@ stage_dir = Bkmkr::Project.input_file.split(Regexp.union(*[File::SEPARATOR, File
 # Finding imprint name
 # imprint = File.read(Bkmkr::Paths.outputtmp_html).scan(/<p class="TitlepageImprintLineimp">.*?</).to_s.gsub(/\["<p class=\\"TitlepageImprintLineimp\\">/,"").gsub(/"\]/,"").gsub(/</,"")
 # Manually populating for now, until we get the DB set up
-if !metaimprint.nil? or !metaimprint.empty?
+if !metaimprint.blank?
 	imprint = metaimprint
 elsif myhash['book'].nil? or myhash['book'].empty? or !myhash['book'] or myhash["book"]["IMPRINT_DESC"].nil? or myhash["book"]["IMPRINT_DESC"].empty? or !myhash["book"]["IMPRINT_DESC"]
 	if project_dir == "torDOTcom"
@@ -176,7 +176,7 @@ else
 	imprint = imprint.encode('utf-8')
 end
 
-if !metapublisher.nil? or !metapublisher.empty?
+if !metapublisher.blank?
 	publisher = metapublisher
 else 
 	publisher = imprint
