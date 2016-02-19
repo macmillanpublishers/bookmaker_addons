958e73f55f19c684760eb3c5895b82c4ccc1773e
nelliemckesson <nellie.mckesson@macmillan.com>
2016-02-19 17:17:34 -0500

    creating dummy metadata for title handling

--- htmlmaker_preprocessing.rb
+++ htmlmaker_preprocessing.rb
@@ -5,3 +5,32 @@ require_relative '../bookmaker/core/header.rb'
 # These commands should run immediately prior to htmlmaker
 doctodocx = "S:\\resources\\bookmaker_scripts\\bookmaker_addons\\htmlmaker_preprocessing.ps1"
 `PowerShell -NoProfile -ExecutionPolicy Bypass -Command "#{doctodocx} '#{Bkmkr::Paths.project_tmp_file}'"`
+
+# Create a temp JSON file
+configfile = File.join(Bkmkr::Paths.project_tmp_dir, "config.json")
+
+datahash = {}
+datahash.merge!(title: "TK")
+datahash.merge!(subtitle: "TK")
+datahash.merge!(author: "TK")
+datahash.merge!(productid: "TK")
+datahash.merge!(printid: "TK")
+datahash.merge!(ebookid: "TK")
+datahash.merge!(imprint: "TK")
+datahash.merge!(publisher: "TK")
+datahash.merge!(project: "TK")
+datahash.merge!(stage: "TK")
+datahash.merge!(printcss: "TK")
+datahash.merge!(printjs: "TK")
+datahash.merge!(ebookcss: "TK")
+datahash.merge!(pod_toc: "TK")
+datahash.merge!(frontcover: "TK")
+datahash.merge!(epubtitlepage: "TK")
+datahash.merge!(podtitlepage: "TK")
+
+finaljson = JSON.generate(datahash)
+
+# Printing the final JSON object
+File.open(configfile, 'w+:UTF-8') do |f|
+  f.puts finaljson
+end
