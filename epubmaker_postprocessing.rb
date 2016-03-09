94c9cbd29cf8247d5b34f58b058e7cfa4d8c9ed2
nelliemckesson <nellie.mckesson@macmillan.com>
2016-03-09 10:13:08 -0500

    adding epubcheck

--- epubmaker_postprocessing.rb
+++ epubmaker_postprocessing.rb
@@ -3,19 +3,23 @@ require 'fileutils'
 require_relative '../bookmaker/core/header.rb'
 require_relative '../bookmaker/core/metadata.rb'
 
-# These commands should run immediately after to epubmaker
+# ---------------------- VARIABLES
 
-configfile = File.join(Bkmkr::Paths.project_tmp_dir, "config.json")
-file = File.read(configfile)
-data_hash = JSON.parse(file)
+data_hash = Mcmlln::Tools.readjson(Metadata.configfile)
 
 OEBPS_dir = File.join(Bkmkr::Paths.project_tmp_dir, "OEBPS")
+
 zipepub_py = File.join(Bkmkr::Paths.core_dir, "epubmaker", "zipepub.py")
 
+# path to epubcheck
+epubcheck = File.join(Bkmkr::Paths.core_dir, "epubmaker", "epubcheck", "epubcheck.jar")
+
+# ---------------------- METHODS
+
+# ---------------------- PROCESSES
 # Add links back to TOC to chapter heads
 searchdir = File.join(OEBPS_dir, "ch[0-9][0-9]*.html")
 chapfiles = Dir.glob(searchdir)
-
 chapfiles.each do |c|
 	replace = File.read(c).gsub(/(<section data-type="chapter".*?><h1.*?>)(.*?)(<\/h1>)/, "\\1<a href=\"toc01.html\">\\2</a>\\3")
 	File.open(c, "w") {|file| file.puts replace}
@@ -52,14 +56,18 @@ if File.file?(podtitlepagetmp)
 	FileUtils.rm(podtitlepagetmp)
 end
 
-csfilename = "#{Metadata.eisbn}_EPUB"
+# Renames final epub for coresource based on stage
+if data_hash['stage'].include? "egalley" or data_hash['stage'].include? "firstpass"
+  csfilename = "#{Metadata.eisbn}_EPUBfirstpass"
+else
+  csfilename = "#{Metadata.eisbn}_EPUB"
+end
 
 # zip epub
 Bkmkr::Tools.runpython(zipepub_py, "#{csfilename}.epub #{Bkmkr::Paths.project_tmp_dir}")
 
 FileUtils.cp("#{Bkmkr::Paths.project_tmp_dir}/#{csfilename}.epub", "#{Bkmkr::Paths.done_dir}/#{Metadata.pisbn}")
 
-# Renames final epub for firstpass
-if data_hash['stage'].include? "egalley" or data_hash['stage'].include? "firstpass"
-  FileUtils.mv("#{Bkmkr::Paths.done_dir}/#{Metadata.pisbn}/#{Metadata.eisbn}_EPUB.epub", "#{Bkmkr::Paths.done_dir}/#{Metadata.pisbn}/#{Metadata.eisbn}_EPUBfirstpass.epub")
-end
+# validate epub file
+puts "---RUNNING EPUBCHECK FOR EPUBMAKER_POSTPROCESSING.RB---"
+Bkmkr::Tools.runjar(epubcheck, "#{final_dir}/#{csfilename}.epub")
