68c3f7c1e152c74b8b1adf6e03ef1a39a61d2ecc
nelliemckesson <nellie.mckesson@macmillan.com>
2016-04-26 09:43:48 -0400

    adding handling for multiple authors in epub

--- epubmaker_preprocessing.rb
+++ epubmaker_preprocessing.rb
@@ -203,11 +203,8 @@ if linkauthorarr.count > 1
   end
   newsletterlink = newslinkarr.join(" ")
   # remove old link
-  oldlink = "<p style=\"text-align: center; text-indent: 0;\">For email updates on the author, click <a href=\"http://us.macmillan.com/authoralerts?authorName={{AUTHORNAMETXT}}&amp;authorRefId={{AUTHORID}}&amp;utm_source=ebook&amp;utm_medium=adcard&amp;utm_term=ebookreaders&amp;utm_content={{AUTHORNAME}}_authoralertsignup_macdotcom&amp;utm_campaign=\{\{EISBN\}\}\">here.</a></p>"
-  puts oldlink
-  mytest = filecontents.scan(oldlink)
-  puts mytest
-  filecontents = filecontents.gsub(/#{oldlink}/, newsletterlink)
+  oldlink = "<p style=\"text-align: center; text-indent: 0;\">For email updates on the author, click <a href=\"\S*\">here.</a></p>"
+  filecontents = filecontents.gsub(/<p style=\"text-align: center; text-indent: 0;\">For email updates on the author, click <a href=\"\S*?\">here.<\/a><\/p>/, newsletterlink)
 else
   linkauthornametxt = linkauthorarr.to_s.downcase.gsub(/\s/,"").gsub(/\W/,"").to_ascii
   linkauthornameall = linkauthorarr.to_s.downcase.gsub(/\s/,"").to_ascii
