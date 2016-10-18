d5f0c8405ca7db5e11ad0f7647aed0a7504ffea8
nelliemckesson <nellie.mckesson@macmillan.com>
2016-10-18 16:13:29 -0400

    restoring allworks

--- metadata_preprocessing.rb
+++ metadata_preprocessing.rb
@@ -50,7 +50,7 @@ end
 # for logging purposes
 puts "RUNNING METADATA_PREPROCESSING"
 
-pisbn, eisbn = findBookISBNs(Bkmkr::Paths.outputtmp_html, Bkmkr::Project.filename)
+pisbn, eisbn, allworks = findBookISBNs(Bkmkr::Paths.outputtmp_html, Bkmkr::Project.filename)
 
 # find titlepage images
 allimg = File.join(Bkmkr::Paths.submitted_images, "*")
