82e0324eeb81b2d0d88ba61c46c1fb0f71f181b0
mattretzer <matthew.retzer@macmillan.com>
2017-06-19 11:34:15 -0400

    one option (all ruby) for fixing the hyphen strings in url issue

--- htmlmaker_postprocessing.rb
+++ htmlmaker_postprocessing.rb
@@ -42,13 +42,31 @@ end
 
 def fixLongHyphenatedWords(html, logkey='')
   filecontents = html
+  # tag & alter long-hyphen phrases that are parts or hyperlinks (so they are bypassed by the next block)
+  longhyphenhyperlinks = html.scan(/(<a href="(?:(?!<a href).)*?(([a-zA-Z]+-){4,}).*?<\/a>)/)
+  longhyphenhyperlinks.each do |lh|
+    source = lh[0]
+    newstring = lh[0].gsub(/(^.*$)/, 'LONGHYPHENHYPERLINK\1ENDLONGHYPHENHYPERLINK').gsub(/-/, "zzzzz") #(placeholders for easy cleanup gsubs)
+    filecontents = filecontents.gsub(source, newstring)
+  end
+
+  # capture and replace all other long-hyphen phrases
   longstrings = html.scan(/(([a-zA-Z]+-){4,})/)
   longstrings.each do |l|
     source = l[0]
     newstring = l[0].gsub(/-/, "<span style='font-size: 2pt;'> </span>-<span style='font-size: 2pt;'> </span>")
     filecontents = filecontents.gsub(source, newstring)
   end
-  return filecontents  
+
+  # return long-hyphen-hypen strings in hyperlinks to their previous state
+  taggedhyphenhyperlinks = filecontents.scan(/LONGHYPHENHYPERLINK.*?ENDLONGHYPHENHYPERLINK/)
+  taggedhyphenhyperlinks.each do |th|
+    source = th
+    newstring = th.gsub(/LONGHYPHENHYPERLINK(.*?)ENDLONGHYPHENHYPERLINK/, '\1').gsub(/zzzzz/, "-") #(placeholders for easy cleanup gsubs)
+    filecontents = filecontents.gsub(source, newstring)
+  end
+
+  return filecontents
 rescue => logstring
   return ''
 ensure
