9ad4d90958b82323fd7373449f7ba0942949a675
mattretzer <matthew.retzer@macmillan.com>
2017-10-11 18:28:01 -0400

    patch to fix newfound bug. pending tests

--- htmlmaker_postprocessing.js
+++ htmlmaker_postprocessing.js
@@ -132,7 +132,7 @@ fs.readFile(file, function editContent (err, contents) {
         }
         // Replace hyphens in any child elements within the para
        $(this).find("*").each(function () {
-          $(this).html( $(this).html().replace(/-/g,"<span class='longhyphenhelper' style='font-size: 2pt; vertical-align:top;'> </span>-<span class='longhyphenhelper' style='font-size: 2pt; vertical-align:top;'> </span>") );
+          $(this).text( $(this).text().replace(/-/g,"<span class='longhyphenhelper' style='font-size: 2pt; vertical-align:top;'> </span>-<span class='longhyphenhelper' style='font-size: 2pt; vertical-align:top;'> </span>") );
         });
         // now remove any instances of those spans that are enclosed in a hyperlinkspan. Have to do this in two steps; previously using a 'not' selector,
         //  but the 'not' was not accounting for nested spans with hyperlink spans at different depths.
