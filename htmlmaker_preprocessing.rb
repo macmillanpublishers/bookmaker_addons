ea9a51c35fb32953da88c6efc7586c6dbb10f29c
mattretzer <matthew.retzer@macmillan.com>
2017-12-13 15:44:05 -0500

    first draft replacing copyright symbol

--- htmlmaker_preprocessing.rb
+++ htmlmaker_preprocessing.rb
@@ -13,6 +13,8 @@ configfile = File.join(Bkmkr::Paths.project_tmp_dir, "config.json")
 
 get_template_version_py = File.join(Bkmkr::Paths.scripts_dir, "bookmaker_addons", "getTemplateVersion.py")
 
+replace_wsym_py = File.join(Bkmkr::Paths.scripts_dir, "bookmaker_addons", "replace_wsym.py")
+
 # ---------------------- METHODS
 
 def convertDocToDocxPSscript(filetype, logkey='')
@@ -42,6 +44,20 @@ ensure
   Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
 end
 
+def replace_wsym(filetype, replace_wsym_py, wsymcode, replacementcode, logkey='')
+  unless filetype == "html"
+    # the replace_wsym_py script checks document.xml without unzipping the .docx. If w:sym with wsymcode is found,
+    #   the w:sym element is replaced with with the decoded replacementcode in the xml, the new xml is overwritten to file,
+    #   the original file is backed up, and the .docx is overwritten with edits.
+    logstring = Bkmkr::Tools.runpython(replace_wsym_py, "#{Bkmkr::Paths.project_docx_file} #{wsymcode} #{replacementcode}")
+  else
+    logstring = 'input file is html, skipping'
+  end
+rescue => logstring
+ensure
+  Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
+end
+
 def writeConfigJson(hash, json, logkey='')
   Mcmlln::Tools.write_json(hash, json)
 rescue => logstring
@@ -57,6 +73,16 @@ convertDocToDocxPSscript(filetype, 'convert_doc_to_docx')
 # get document version template number if it exists
 template_version = checktemplate_version(filetype, get_template_version_py, 'check_docx_template_version')
 @log_hash['template_version'] = template_version
+
+@log_hash['hi'] = 'hi'
+# run replacements on any w:sym elements in the word xml:
+# Right now, just to catch a copyright symbol variant, but for additional replacements, just run the method again with codes
+#   wsymcode values can be found in the xml (value of 'w:char attribute for the w:sym'), here is a large table of these codes:
+#     https://gist.github.com/ptsefton/1ce30879e9cfef289356
+#   replacement code should be unicode for desired replacement symbol: http://www.fileformat.info/info/unicode/char/search.htm
+#     the desired format is the 'Python source code' version, sans the preceding 'u'
+replace_wsym(filetype, replace_wsym_py, 'F0D3', '\u00A9', 'replace_w:sym_copyright_symbol')
+
 
 # Create a temp JSON file
 datahash = {}
