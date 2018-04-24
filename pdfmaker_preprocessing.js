d4e6f72c59012b513cfb52100a6616f34a0c15ca
mattretzer <matthew.retzer@macmillan.com>
2018-04-23 22:58:29 -0400

    added spans to hyphenated items

--- pdfmaker_preprocessing.js
+++ pdfmaker_preprocessing.js
@@ -1,16 +1,23 @@
 var fs = require('fs');
 var cheerio = require('cheerio');
 var file = process.argv[2];
-var booktitle = process.argv[3]; 
+var booktitle = process.argv[3];
 var bookauthor = process.argv[4];
 var pisbn = process.argv[5];
 var imprint = process.argv[6];
 var publisher = process.argv[7];
+var jsdom = require('jsdom');
+
+var document = ''//jsdom.jsdom(src);
 
 fs.readFile(file, function editContent (err, contents) {
+  // console.log(contents.toString())
+  document = jsdom.jsdom(contents.toString());
   $ = cheerio.load(contents, {
           xmlMode: true
         });
+
+// var document = jsdom.jsdom($);
 
 // add titlepage image if applicable
   if ($('section[data-titlepage="yes"]').length) {
@@ -46,6 +53,69 @@ fs.readFile(file, function editContent (err, contents) {
           return console.log(err);
       }
 
-      console.log("Content has been updated!");
+      console.log("Content has been updated! (cheerio)");
   });
+
+
+  // scan the paragraphs for text containing hyphenated phrases
+
+  // var ps = $("p");
+  var ps = document.getElementsByTagName("p");
+  var ts = [];
+
+  for (var i = 0; i < ps.length; ++i) {
+      var p = ps[i];
+      checkText(p, ts);
+  }
+
+  function checkText(e, ts) {
+      for (var c = e.firstChild; c; c = c.nextSibling) {
+        // if (c.nodeType == Node.ELEMENT_NODE) {
+          if (c.nodeType == 1) {  // 1: Node.ELEMENT_NODE
+              // recurse down through inline elements
+              checkText(c, ts);
+        // } else if (c.nodeType == Node.TEXT_NODE) {
+          } else if (c.nodeType == 3) {   //3:  Node.TEXT_NODE
+              // the regular expression needs to be extended for non-Latin
+              // scripts, also \w does not match accented characters
+              if (c.data.match(/\w-\w/)) {
+                  ts.push(c);
+              }
+          }
+      }
+  }
+
+  for (var i = 0; i < ts.length; ++i) {
+      var t = ts[i];
+      // repeats as text node can contain multiple phrases
+      while (t) {
+          // match entire phrase from beginning to end
+          var index = t.data.search(/(-)(\b\w{3,6})|(\w{3,6}\b)(?=-)/);
+          // (orig regex: (/\w+-\w[-\w]*/); this only captures
+          if (index < 0) break;
+          var t2 = t.splitText(index);
+          // find the end of the phrase
+          index = t2.data.search(/[^-\w]/);
+          if (index > 0) {
+              t = t2.splitText(index);
+          } else {
+              t = null;
+          }
+          // wrap the phrase in a span element
+          var span = document.createElement("span");
+          t2.parentNode.insertBefore(span, t2);
+          span.appendChild(t2);
+          span.className = "prevent_hyphen";
+          // console.log("hyphenated phrase: "+t2.data);
+      }
+  }
+    var output = document.documentElement.outerHTML;
+  fs.writeFile(file, output, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("Content has been updated! (jsdom)");
+});
+
 });
