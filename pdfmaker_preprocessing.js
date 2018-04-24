d715f208f6a5528943bf0be32dfe6372d5dd0068
mattretzer <matthew.retzer@macmillan.com>
2018-04-24 17:01:44 -0400

    cleaned up, fixed dependencies

--- pdfmaker_preprocessing.js
+++ pdfmaker_preprocessing.js
@@ -11,17 +11,11 @@ var jsdom = require('jsdom');
 var document = ''//jsdom.jsdom(src);
 
 fs.readFile(file, function editContent (err, contents) {
-  // console.log(contents.toString())
-  document = jsdom.jsdom(contents.toString());
-  // const {JSDOM} = jsdom;
-// const {document} = (new JSDOM(contents.toString())).window;
-// const JsDOM = require('jsdom').JSDOM;
-// let dom = new JsDOM(contents);
+  const {JSDOM} = jsdom;
+  const {document} = (new JSDOM(contents.toString())).window;
   $ = cheerio.load(contents, {
           xmlMode: true
         });
-
-// var document = jsdom.jsdom($);
 
 // add titlepage image if applicable
   if ($('section[data-titlepage="yes"]').length) {
@@ -62,8 +56,6 @@ fs.readFile(file, function editContent (err, contents) {
 
 
   // scan the paragraphs for text containing hyphenated phrases
-
-  // var ps = $("p");
   var ps = document.getElementsByTagName("p");
   var ts = [];
 
@@ -74,11 +66,9 @@ fs.readFile(file, function editContent (err, contents) {
 
   function checkText(e, ts) {
       for (var c = e.firstChild; c; c = c.nextSibling) {
-        // if (c.nodeType == Node.ELEMENT_NODE) {
-          if (c.nodeType == 1) {  // 1: Node.ELEMENT_NODE
+          if (c.nodeType == 1 && c.className != 'spanISBNisbn') {  // 1: Node.ELEMENT_NODE
               // recurse down through inline elements
               checkText(c, ts);
-        // } else if (c.nodeType == Node.TEXT_NODE) {
           } else if (c.nodeType == 3) {   //3:  Node.TEXT_NODE
               // the regular expression needs to be extended for non-Latin
               // scripts, also \w does not match accented characters
@@ -94,42 +84,29 @@ fs.readFile(file, function editContent (err, contents) {
       // repeats as text node can contain multiple phrases
       while (t) {
           // match entire phrase from beginning to end
-          var index = t.data.search(/(-)(\b\w{4,6})|(\w{4,6}\b)(?=-)/);
-          var re = /(-)(\b\w{3,6})|(\w{3,6}\b)(?=-)/;
+          var re = /(-)(\b\w{4,6})|(\w{4,6}\b)(?=-)/;
           var result = re.exec(t.data);
 
-          if (result == null) break;
+          if (result == null || /\bISBN\b/.test(result[0])) break;
+          // beginning of phrase
           firstindex = result.index
+          // end of phrase (relative)
           lastindex = result[0].length
-          // lastindex = result.lastindex
-// console.log(firstindex,lastindex)
-          // (orig regex: (/\w+-\w[-\w]*/); this only captures
-          if (index < 0) break;
 
-                    // console.log(firstindex,lastindex)
-          // var t2 = t.splitText(index);
-          // find the end of the phrase
-
-          // var firstmatch=t2.data.match(/(-)(\b\w{3,6})|(\w{3,6}\b)(?=-)/)
-
+          // split phrase
           var t2=t.splitText(firstindex);
-          // var q=q2.splitText(lastindex);
-
-          // index = t2.data.search(/[^-\w]/);//(/[^-\w]/);
           if (lastindex > 0) {
               t = t2.splitText(lastindex);
           } else {
               t = null;
           }
-          // var q2=t.splitText(firstindex);
-          // var q=q2.splitText(lastindex);
 
           // wrap the phrase in a span element
           var span = document.createElement("span");
           t2.parentNode.insertBefore(span, t2);
           span.appendChild(t2);
           span.className = "prevent_hyphen";
-          console.log("hyphenated phrase: "+t2.data);
+          // console.log("hyphenated phrase: "+t2.data);
       }
   }
     var output = document.documentElement.outerHTML;
