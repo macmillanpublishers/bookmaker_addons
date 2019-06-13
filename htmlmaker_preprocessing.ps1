8f4c5045fe0210d849c50eddf550f6765c92b230
MacmillanWorkflows <workflows@macmillan.com>
2019-06-13 14:58:07 -0400

    added update to tmpdir path for ps1 script

--- htmlmaker_preprocessing.ps1
+++ htmlmaker_preprocessing.ps1
@@ -1,7 +1,7 @@
 ﻿# Converts a .doc file in bookmaker_tmp to .docx, and removes any
 # revision info in the XML file for .docx if you have "Store random numbers
 # to improve Combine accuracy" unchecked already.
-# you can pass the original file location in convert, 
+# you can pass the original file location in convert,
 # but you have to run tmparchive.rb first
 # and of course set the correct location of log dir and bookmaker_tmp below
 
@@ -14,8 +14,9 @@ $tmpDir="/bookmaker_tmp"
 
 # getting the path inputs
 $currVolPath=Get-Location
-$currVol=split-path $currVolPath -Qualifier	#C: or S:	
+$currVol=split-path $currVolPath -Qualifier	#C: or S:
 $filenameSplit=split-path $inputFile -Leaf			#file name without path
+$uniqueTmpDir=split-path(split-path $inputFile -Parent) -Leaf
 Write-Host "Input file is $filenameSplit"
 $filename=$filenameSplit.SubString(0, $filenameSplit.LastIndexOf('.')).replace(' ','')	#filename w/out extension or spaces
 $fileType=$filenameSplit.SubString($filenameSplit.LastIndexOf('.'))  # just extension
@@ -23,7 +24,7 @@ $subfolder=$inputFile -match "(?:bookmaker\w+)(?<imprint>/.+?/)"    # regex to m
 $imprintPath=$matches["imprint"]                             # returns match from previous line
 
 # put it all together for tmp path
-$folderpath=echo $($currVol + $tmpDir + $imprintPath + $filename + "/")
+$folderpath=echo $($currVol + $tmpDir + $imprintPath + $uniqueTmpDir + "/")
 $docpath=echo $($folderpath + $filename)  #with doc name, w/o extension
 
 # define log file
@@ -46,7 +47,7 @@ $SaveFormat = "microsoft.office.interop.word.WdSaveFormat" -as [type]
 $word = New-Object -ComObject word.application
 $word.visible = $false
 
-# Make sure "Store random number..." setting is off 
+# Make sure "Store random number..." setting is off
 $word.Options.StoreRSIDOnSave = $false
 Write-Host "Removing revision data"
 
@@ -66,15 +67,15 @@ Foreach($comment in $doc.Comments)
 # remove any revision data in XML file, so set 'Saved' property to false
 $doc.Saved=$false
 $wdFormatDocx = 16  # wdFormatDocumentDefault is docx, reference number is 16
-	
-# Have to add [ref]s for certain versions of powershell (2.0), 
+
+# Have to add [ref]s for certain versions of powershell (2.0),
 # we'll see which way works on server
 # https://richardspowershellblog.wordpress.com/2012/10/15/powershell-3-and-word/
 # so only use one or the other of these next two lines
 $saved=$doc.Saved
 #$doc.saveas($docpath, $wdFormatDocx)
 $doc.saveas([ref]$docpath, [ref]$wdFormatDocx)
-	
+
 $doc.close()
 $word.Quit()
 $word = $null
@@ -92,7 +93,7 @@ Else {LogWrite "FAIL: original filename is not null"}
 
 # filename.docx should exist in tmp conversion dir
 $ChkFile = $($docpath + ".docx")
-$FileExists = Test-Path $ChkFile 
+$FileExists = Test-Path $ChkFile
 If ($FileExists -eq $True) {LogWrite "pass: $filename.docx exists in $folderpath"}
 Else {LogWrite "FAIL: $filename.docx exists in $folderpath"}
 
